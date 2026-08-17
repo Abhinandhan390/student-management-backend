647c664e1b4cfb0320693c360c3cc8e9e8f2d6e2
Abhinandhan390 <iyengarabhi390@gmail.com>
2026-08-17 16:09:53 +0530

    Connect backend to local MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = 5000;
+
+connectDB();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('PoCP Backend is running');
